b3d1a4220d87bc80bfb5aac6e3563ebd7f024615
StanimirSofronovMG <stanimir.sofronov@gmail.com>
2022-06-15 14:31:12 +0300

    changes

--- BestBlog/BusinessLayer/Comment.cs
+++ BestBlog/BusinessLayer/Comment.cs
@@ -3,13 +3,45 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace BusinessLayer
 {
-     public class Comment
+    public class Comment
     {
         [Key]
         public int ID { get; private set; }
+
+        [Required, MaxLength(32000)]
+        public string Content { get; set; }
+
+        [ForeignKey("Post")]
+        public int PostID { get; set; }
+
+        [Required]
+        public Post Post { get; set; }
+
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        public DateTime Created_On { get; set; }
+
+
+        private Comment()
+        {
+
+        }
+
+        public Comment(string content, Post post, User user)
+        {
+            this.Content = content;
+            this.Post = post;
+            this.User = user;
+            this.Created_On = DateTime.Now;
+        }
     }
 }
--- BestBlog/BusinessLayer/Post.cs
+++ BestBlog/BusinessLayer/Post.cs
@@ -2,13 +2,41 @@
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BusinessLayer
 {
-    public  class Post
+    public class Post
     {
         [Key]
         public int ID { get; private set; }
+
+        [MaxLength(200), Required]
+        public string Title { get; set; }
+
+        [MaxLength(32000)]
+        public string Content { get; set; }
+
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        public DateTime Created_On { get; set; }
+
+        public IEnumerable<Comment> Comments { get; set; }
+
+        private Post()
+        {
+
+        }
+
+        public Post(User user, string title)
+        {
+            this.User = user;
+            this.Title = title;
+            this.Created_On = DateTime.Now;
+        }
     }
 }
