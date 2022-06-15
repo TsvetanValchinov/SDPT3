1ecf9aa79f19fb7466314d2b3b194d20b9ad5354
StanimirSofronovMG <stanimir.sofronov@gmail.com>
2022-06-15 14:40:42 +0300

    BestBlogContext + IDB

--- BestBlog/DataLayer/BestBlogDBContext.cs
+++ BestBlog/DataLayer/BestBlogDBContext.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using BusinessLayer;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataLayer
 {
-    class BestBlogDBContext
+    class BestBlogDBContext : DbContext
     {
+        public BestBlogDBContext()
+        {
+
+        }
+
+        public BestBlogDBContext(DbContextOptions options) 
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-JARQ3N6;Database=StanimirSofronov_30.05;Trusted_Connection=True;");
+            }
+        }
+
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
     }
 }
