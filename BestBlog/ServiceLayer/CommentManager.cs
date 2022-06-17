467980518a169bdbed77f650c302e5eeff01dd8d
Цветан Вълчинов <cecovalchinov@abv.bg>
2022-06-17 15:21:31 +0300

    added CommentManager and PostManager in ServiceLayer

---
+++ BestBlog/ServiceLayer/CommentManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessLayer;
+using DataLayer;
+
+namespace ServiceLayer.Controllers
+{
+    public class CommentManager : IRepository<Comment, int>
+    {
+        private CommentContext _commentRepository;
+
+        public CommentManager(BestBlogDBContext context)
+        {
+            _commentRepository = new CommentContext(context);
+        }
+
+        public void Create(Comment item)
+        {
+            try
+            {
+                _commentRepository.Create(item);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Comment Read(int key)
+        {
+            try
+            {
+                return _commentRepository.Read(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<Comment> Read(int skip, int take)
+        {
+            try
+            {
+                return _commentRepository.Read(skip, take);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<Comment> ReadAll()
+        {
+            try
+            {
+                return _commentRepository.ReadAll();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(Comment item)
+        {
+            try
+            {
+                _commentRepository.Update(item);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void Delete(int key)
+        {
+            try
+            {
+                _commentRepository.Delete(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ BestBlog/ServiceLayer/PostManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessLayer;
+using DataLayer;
+
+namespace ServiceLayer.Controllers
+{
+    public class PostManager : IRepository<Post, int>
+    {
+        private PostContext _postRepository;
+
+        public PostManager(BestBlogDBContext context)
+        {
+            _postRepository = new PostContext(context);
+        }
+
+        public void Create(Post item)
+        {
+            try
+            {
+                _postRepository.Create(item);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Post Read(int key)
+        {
+            try
+            {
+                return _postRepository.Read(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<Post> Read(int skip, int take)
+        {
+            try
+            {
+                return _postRepository.Read(skip, take);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<Post> ReadAll()
+        {
+            try
+            {
+                return _postRepository.ReadAll();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(Post item)
+        {
+            try
+            {
+                _postRepository.Update(item);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int key)
+        {
+            try
+            {
+                _postRepository.Delete(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
